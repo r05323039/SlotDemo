a5cf305714061ae4974ef2b7c78ff19f3515d683
ian.lin@webcomm.com.tw <ian.lin@webcomm.com.tw>
2023-12-09 13:54:41 +0800

    200 400

--- src/main/java/com/example/slotdemo/service/AllStarPayTable.java
+++ src/main/java/com/example/slotdemo/service/AllStarPayTable.java
@@ -1,8 +1,25 @@
 package com.example.slotdemo.service;
 
+import java.util.List;
+
 public class AllStarPayTable implements PayTable {
     @Override
     public int getOdd(Screen screen) {
-        return 400;
+        List<List<String>> rawScreen = screen.rawScreen();
+
+        int countA = 0;
+        for (List<String> row : rawScreen) {
+            if (row.get(0).equals("A")) {
+                countA++;
+            } else {
+                break;
+            }
+        }
+
+        if (countA ==5){
+            return 400;
+        }
+
+        return 200;
     }
 }
--- src/test/java/com/example/slotdemo/service/AllStarPayTableTest.java
+++ src/test/java/com/example/slotdemo/service/AllStarPayTableTest.java
@@ -22,6 +22,22 @@ public class AllStarPayTableTest {
         int actual = sut.getOdd(screen);
 
         Assertions.assertThat(actual).isEqualTo(400);
+    }
 
+    @Test
+    void A_4_200() {
+        PayTable sut = new AllStarPayTable();
+
+        Screen screen = new Screen(List.of(
+                List.of("A", "1", "2"),
+                List.of("A", "2", "5"),
+                List.of("A", "1", "3"),
+                List.of("A", "3", "4"),
+                List.of("2", "1", "2")
+        ));
+
+        int actual = sut.getOdd(screen);
+
+        Assertions.assertThat(actual).isEqualTo(200);
     }
 }
